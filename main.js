7636029c2ff1311b22c1b257a12bdddbd122613f
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2020-10-05 10:33:52 -0400

    completed several loop functions

---
+++ main.js
@@ -0,0 +1,89 @@
+
+function challenge0() {
+    let num = 1;
+    while (num <= 5) {
+        console.log (num);
+        num = num + 1;
+    }
+}
+// prints out 1-5
+challenge0 ()
+
+
+function challenge1() {
+    let num = 1;
+    while (num <= 3) {
+        console.log(num);
+        num = num + 1;
+    }
+}
+// prints out 1-3 or 1-6 when num is changed to 6.
+challenge1()
+
+
+function challenge2() {
+    let num = 3;
+    while (num <= 8) {
+        console.log(num);
+        num = num + 1;
+    } 
+}
+function challenge3 () {
+    let num = -2;
+    while (num <= 1) {
+        console.log (num);
+        num = num + 1;
+    }
+}
+// // could not figure out how to call it within the same function.
+challenge2 ()
+challenge3 () 
+
+function challenge4 () {
+    let num = 2
+    while (num <= 10) {
+        console.log (num);
+        num = num + 2;
+    }
+}
+// prints out 2,4,6,10
+challenge4 ()
+
+function challenge5 () {
+    let num = 10
+    while (num >= 1) {
+        console.log (num);
+        num = num - 1;
+    }
+}
+// // prints out 10 - 1 in descending order!
+challenge5 ()
+
+function challenge6 () {
+    let num = 10
+    while (num >= 1) {
+        console.log (num);
+        num = num - 1;
+    }
+}
+// 10 - 1 in Descending order prints "Blast Off", at end.
+challenge6 ()
+console.log ("Blast Off")
+
+function challenge7 () {
+    let num = 20
+    while (num >= 1) {
+        console.log (num);
+        num = num -3;
+    }
+}
+// prints out 20,17,14,11,8,5,2
+challenge7 ()
+
+function challenge8 () {
+    let str = Hello
+    while (str <=)
+}
+
+
+
